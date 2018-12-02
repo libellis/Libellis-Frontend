79c85251befd75e97766076c1a4d0284b2a51052
Kevin Welch <kaedub16@gmail.com>
2018-12-02 13:36:37 -0800

    Added a profile route that renders a UserProfile component that hanldes the profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './NavBar';
 import Wall from './Wall';
 import Auth from './Auth';
 import SurveyDetails from './SurveyDetails';
+import UserProfile from './UserProfile';
 import SurveyCreator from './SurveyCreator';
 import LoginSignupForm from './LoginSignupForm';
 import {connect} from 'react-redux';
@@ -27,6 +28,11 @@ class App extends Component {
               exact
               path="/create"
               render={props => <SurveyCreator {...props} />}
+            />
+            <Route
+              exact
+              path="/profile"
+              render={props => <UserProfile {...props} />}
             />
             <Route exact path="/login" render={() => <LoginSignupForm />} />
           </Switch>
